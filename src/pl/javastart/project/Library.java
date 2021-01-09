30a1bb5f7a5135c8736ab2337287348b23b815f0
Przemek Bus <gramp.fot@gmail.com>
2021-01-09 19:20:11 +0100

    Added two more books

--- src/pl/javastart/project/Library.java
+++ src/pl/javastart/project/Library.java
@@ -7,6 +7,13 @@ public class Library {
         Book book1 = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",
                 2010,296,"Greg","9788373271890");
 
+        Book book2 = new Book("Java. Efektywe programowanie. Wydanie II","Joshua Bloch",
+                2009,352,"Helion","9788324620845");
+
+        Book book3 = new Book("SCJP Sun Certified Programer for Java 6 Study Guide","Bert Bates, Katherine Sierra",
+                2008,851,"McGrawm-Hill Osborne Media","9780071591065");
+
+
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
         System.out.println(book1.title);
