0c27b7feaaf5d2f01eff93863b986afff20e1594
Przemek Bus <gramp.fot@gmail.com>
2021-01-09 19:19:41 +0100

    Updated Book costructor

--- src/pl/javastart/project/Library.java
+++ src/pl/javastart/project/Library.java
@@ -4,14 +4,8 @@ public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka v0.2";
 
-        Book book1 = new Book();
-
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.releaseDate = 2010;
-        book1.pages = 296;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373271890";
+        Book book1 = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",
+                2010,296,"Greg","9788373271890");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
